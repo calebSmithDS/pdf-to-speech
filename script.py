2a25e5057a41c18b19ab46a677fb5c868301e1d5
calebSmithDS <109048857+calebSmithDS@users.noreply.github.com>
2024-12-26 13:30:20 +1030

    added filedialog box - added pdf-mp3 convert - added better comments

--- script.py
+++ script.py
@@ -2,21 +2,27 @@ import re
 from pdfminer.high_level import extract_text
 import unicodedata
 import sys
+import pyttsx3
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
 
 # this is basically setting the default output to utf-8, a lot of computers default to charmap
 sys.stdout.reconfigure(encoding='utf-8')
 
-
+# extracts the pdf document - creates a txt file and a mp3 file
 def extract_text2(pdf):
-   
-
+    # extract text from pdf
     text = extract_text(pdf)
+    # intialize the speaker
+    speaker = pyttsx3.init()
 
 # normalize text
     text = unicodedata.normalize("NFKC", text)
+# strip newline characters from text - dont think working atm
+    text = text.strip().replace('/n',' ')
 
-    # refReg = re.compile(r'[\r\n]')
-    # newtext = re.sub(refReg, '', text)  
+# pattern matching and removing 
     refReg = re.compile(r'\(.+[,]\s*\d+\)')
     newtext = re.sub(refReg, '', text)
     refReg = re.compile(r'\[.+\]')
@@ -29,32 +35,44 @@ def extract_text2(pdf):
     refReg = re.compile(r'\(.+[,]\s*\n\d+\)')
     newtext = re.sub(refReg, '', newtext)   
 
-    # refReg = re.compile(r'\(.+[,].*\d+\)', re.DOTALL)
-    # newtext = re.sub(refReg, '', newtext) 
-
     # references
     refReg = re.compile(r'References\s*.*', re.DOTALL)
     newtext = re.sub(refReg, '', newtext)  
 
-    # txt_file = "output.txt"
+    # naming the files 
+    mp3_file = pdf + "-output.mp3"
     txt_file = pdf + "-output.txt"
 
+    # creating mp3 file
+    speaker.save_to_file(newtext, mp3_file)
+    speaker.runAndWait()
+    speaker.stop()
+
+    # creating text file
     with open(txt_file, 'w', encoding="utf-8", errors='replace') as txtfile:
         txtfile.write(newtext)
 
-
+def open_file():
+    # getting file path and opening window
+    filename = filedialog.askopenfilename()
+    return filename
 
 try:
 # this is used to take in arguments from the command line
-    file = sys.argv[1]
-    extract_text2(file)
+    # file = sys.argv[1]
+    # extract_text2(file)
 
 # development and debug 
-    # extract_text2("1-s2.0-S030646032300326X-main (1).pdf")
+    file = open_file()
+    extract_text2(file)
 # prints to screen for debugging purposes
-    # with open('output.txt', 'r', encoding="utf-8", errors='replace') as file:
-    #     content = file.read()
-    #     # print(content)
+    with open('output.txt', 'r', encoding="utf-8", errors='replace') as file:
+        content = file.read()
+        print(content)
+    # creating the tkinter gui window
+    root = Tk()
+    root.title("PDF to MP3")
+    root.mainloop()
 
 except Exception as e:
     print(f"Error reading file: {e}")
